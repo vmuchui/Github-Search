74bb4e7cd726a48e4c9ec94b345a21dfe3c49008
vmuchui <kaigongimuchui@gmail.com>
2019-05-08 08:09:41 +0300

    added the actual imported modules.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,30 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Profile } from './profiles/profiles';
+import { RepositoriesComponent } from './repos/repos.component';
+
+import {HttpClientModule} from '@angular/common/http';
+import {FormsModule} from '@angular/forms';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { HighlightDirective } from './highlight.directive';
+import { ProfilesComponent } from './profiles/profiles.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProfilesComponent,
+    RepositoriesComponent,
+    NotFoundComponent,
+    HighlightDirective,
+    RepositoriesComponent,
+    ProfilesComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
